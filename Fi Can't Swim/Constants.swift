2aec29f7b20ed6ef09cb24bdc6144e97eda01509
imaginarycow <beltran.ramiro@hotmail.com>
2017-02-20 16:35:27 -0800

    End Of Day

--- Fi Can't Swim/Constants.swift
+++ Fi Can't Swim/Constants.swift
@@ -12,3 +12,7 @@ import SpriteKit
 
 let TitlePosition = CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height * 0.9)
 let BackButtonPosition = CGPoint(x: UIScreen.main.bounds.width * 0.1, y: UIScreen.main.bounds.height * 0.9)
+let centerX = CGFloat(UIScreen.main.bounds.width / 2)
+let centerY = CGFloat(UIScreen.main.bounds.height / 2)
+
+let NumberOfLevels: Int = 25
--- Fi Can't Swim/MapScene.swift
+++ Fi Can't Swim/MapScene.swift
@@ -10,12 +10,14 @@ import SpriteKit
 
 class MapScene : SKScene {
     
-    let title = Label(label: "Settings", fontColor: .red, fontSize: 30.0)
+    let title = Label(label: "Map", fontColor: .red, fontSize: 30.0)
     let backButton = BackButton()
+    var levelIcons: [LevelIcon] = []
     
     override func didMove(to view: SKView) {
         super.didMove(to: view)
         
+        deviceWidth = Int(self.view!.bounds.size.width)
         backgroundColor = .white
         title.position = CGPoint(x: (self.view?.bounds.width)! / 2, y: (self.view?.bounds.height)! * 0.9)
         title.zPosition = 10
@@ -24,9 +26,43 @@ class MapScene : SKScene {
         backButton.position = BackButtonPosition //CGPoint(x: (self.view?.bounds.width)! * 0.1, y: (self.view?.bounds.height)! * 0.9)
         self.addChild(backButton)
         
+        showLevels()
+        
     }
     override func willMove(from view: SKView) {
         self.removeAllChildren()
+    }
+    func showLevels(){
+        buildLevels(numberOfLevels: NumberOfLevels)
+        let buffer = CGFloat(10.0)
+        var count:CGFloat = 1
+        var row = 1
+        for icon in levelIcons {
+                
+            icon.position = CGPoint(x: 0.0 + icon.size.width * count + buffer, y: (self.view?.bounds.height)! * 0.8 - row * icon.size.height * 2)
+            self.addChild(icon)
+                    
+            count += 1
+            if count = 6{
+                count = 1
+                row += 1
+            }
+        }
+    
+       connectLevels()     
+    }
+    
+    func connectLevels(){
+        
+    }
+    func buildLevels(numberOfLevels: Int){
+        var index = 1
+        for _ in 1...numberOfLevels {
+            let levelIcon = LevelIcon(levelNum: index)
+            levelIcons.append(levelIcon)
+            index += 1
+        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
